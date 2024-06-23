6588d607acbe9b9e2787f89f01f26b1b27e577d5
FatCatGotRekt <helloworlditsmekrisz@gmail.com>
2024-06-23 08:35:25 +0530

    .

--- frontend/src/pages/Chats.tsx
+++ frontend/src/pages/Chats.tsx
@@ -6,10 +6,19 @@ interface ChatProps {
 
 const Chat: React.FC<ChatProps> = ({ message }) => {
   return (
-    <div className="flex-1 p-4">
+    <div className="flex-1 p-4 flex justify-between flex-col">
+      <div>
       <p className="text-lg font-semibold">Chatting Section</p>
-      <div className="bg-white p-4 border border-gray-300 rounded-full mt-4">
+      <div className="bg-white p-4 border border-gray-300 mt-4 rounded-full">
         <p className="text-gray-600">{message}</p>
+      </div>
+      </div>
+      <div>
+      <input
+          type="text"
+          placeholder="Type a message"
+          className="px-3 py-2 bg-white border border-gray-300 focus:outline-none focus:border-blue-500 w-full rounded-full h-14"
+        />
       </div>
     </div>
   );
